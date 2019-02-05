7b4f212680b6dcda1140fbcefac961801ddec2ea
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2019-02-05 21:05:39 +0100

    Minor update to install.sh

--- py/install.sh
+++ py/install.sh
@@ -2,8 +2,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #get the dir of t
 cd $SCRIPT_DIR #got to it
 
 #install the virtual env
-python3.7 -m venv venv
-source venv/bin/activate
-pip install wheel cluster_vcf_records pandas
-chmod +x py/cleanAnnotatedVcf.py
+python3.7 -m venv venv && \
+source venv/bin/activate && \
+pip install wheel cluster_vcf_records pandas && \
+chmod +x clean_annotated_vcf.py && \
 deactivate
