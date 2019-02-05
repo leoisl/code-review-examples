9cd16df093a28c068422cc5bdcb7392c78681ed9
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2019-02-05 21:18:39 +0100

    Code cleanup

--- py/convert_spaces_into_tabs.py
+++ py/convert_spaces_into_tabs.py
@@ -6,20 +6,3 @@ for line in sys.stdin:
         print(line)
     else:
         print("\t".join(line.split()))
-
-
-
-'''
-import sys
-
-#some care must be taken because cluster_vcf_records bugs on empty lines
-firstLine=True
-for line in sys.stdin:
-    line=line.strip()
-    if not firstLine: print()
-    if line.startswith("#"):
-        print(line, end='')
-    else:
-        print("\t".join(line.split()), end='')
-    firstLine=False
-'''
