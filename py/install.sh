1f88b4b10e62a625f9981a867169f0db25e0a80e
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2019-02-05 20:10:28 +0100

    Updating installation script

--- py/install.sh
+++ py/install.sh
@@ -1,1 +1,9 @@
-pip install wheel cluster_vcf_records
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #get the dir of this script
+cd $SCRIPT_DIR #got to it
+
+#install the virtual env
+python3.7 -m venv venv
+source venv/bin/activate
+pip install wheel cluster_vcf_records pandas
+chmod +x py/cleanAnnotatedVcf.py
+deactivate
